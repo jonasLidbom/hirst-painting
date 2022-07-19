a260151704dedc2e1c2bd885f3ce5aee6904c778
Jonas Lidbom <63039611+jonasLidbom@users.noreply.github.com>
2022-07-19 12:50:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# How I imported the colors in color_list:
-# import colorgram
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-# print(rgb_colors)
-
 import turtle
 from turtle import Turtle, Screen
 import random
